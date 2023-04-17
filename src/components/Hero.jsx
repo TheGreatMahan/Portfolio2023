532ca09e4ea0c2010f14b03c7ef939351256b83d
Mahan <mehdipour.mahan1@gmail.com>
2023-04-16 23:30:22 -0400

    Mobile size for computer is added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,11 +19,30 @@ const Hero = () => {
                         Hi, I'm <span className="text-[#915eff]">Mahan</span>
                     </h1>
                     <div className={`${styles.heroSubText} mt-2 text-white`}>
-                      I develop software, whether it is backend, frontend, <br className="sm:block hidden"/> full-stack, or any type of applications
+                        I develop software, whether it is backend, frontend, <br className="sm:block hidden"/> full-stack, or for any type of applications.
                     </div>
                 </div>
             </div>
             <ComputersCanvas/>
+
+            <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+                <a href='#about'>
+                    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                        <motion.div
+                            animate={{
+                                y: [0,24,0]
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType:'loop'
+                            }}
+                            className='w-3 h-3 rounded-full bg-secondary mb-1'
+                        />
+                    </div>
+                </a>
+            </div>
+
         </section>
     );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
 
   return (
@@ -12,8 +12,8 @@ const Computers = () => {
       <pointLight intensity={0.5}/>
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={ isMobile ? 0.75 : 0.5 }
+        position={isMobile ? [0, -3.00, -1.5] : [0, -1.75, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
       <spotLight 
@@ -29,6 +29,24 @@ const Computers = () => {
 }
 
 const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    } 
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+
   return (
     <Canvas 
       frameloop='demand'
@@ -42,7 +60,7 @@ const ComputerCanvas = () => {
           minPolarAngle={Math.PI / 2}
           enablePan={false}
         />
-        <Computers/>
+        <Computers isMobile={isMobile}/>
       </Suspense>
 
       <Preload all/>
